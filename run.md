8dad0e94b13b9ef3b240012f28da8b47bf8bc2ac
faith <faith@com>
2023-07-19 19:05:44 +0800

    update md

--- run.md
+++ run.md
@@ -87,3 +87,39 @@ Source: Conversation with Bing, 7/19/2023
 
 
 /home/faith/pytorch/aten/src/ATen/native/transformers/attention.cpp
+
+
+
+
+您好，这是Bing。我可以帮您解释一下chatGLM中的top_k、top_p和temperature参数的含义。😊
+
+chatGLM是一种基于GPT系列模型的聊天生成器，它可以根据用户的输入生成自然和有趣的回复。为了控制生成过程中的随机性和多样性，chatGLM提供了一些参数，如top_k、top_p和temperature，让用户可以调整生成的质量和风格。
+
+top_k是一个整数，它表示在每个生成步骤中，模型只考虑最可能的前k个单词作为候选输出。例如，如果top_k设置为3，那么模型只会从概率最高的三个单词中随机选择一个作为输出。top_k越大，生成的多样性越高，但也可能导致一些不合理或不连贯的输出。top_k越小，生成的一致性越高，但也可能导致一些重复或无聊的输出。
+
+top_p是一个浮点数，它表示在每个生成步骤中，模型只考虑概率之和不超过p的最可能的单词作为候选输出。例如，如果top_p设置为0.75，那么模型会从概率最高的单词开始累加，直到概率之和达到或超过0.75为止，然后从这些单词中随机选择一个作为输出。top_p也可以控制生成的多样性和一致性，但它是动态的，因为它根据每个单词的概率来调整候选列表的大小。top_p通常设置为较高的值（如0.75），目的是限制可能被采样的低概率单词的长尾。
+
+temperature是一个浮点数，它表示在每个生成步骤中，模型对每个单词的概率进行重新调整的程度。温度越高，模型对每个单词的概率越平均，这意味着更多的随机性和创造性。温度越低，模型对每个单词的概率越偏向于原始值，这意味着更少的随机性和更多的确定性。温度为0时，模型总是选择概率最高的单词作为输出。
+
+这些参数可以根据不同的任务和场景进行调整，以达到最佳的生成效果。一般来说，执行有“正确”答案的任务（如问答或摘要）时，较低的温度（小于1）和较小的top_k或top_p更合适。执行需要更多创意和多样性的任务（如写故事或歌词）时，较高的温度（大于1）和较大的top_k或top_p更合适。
+
+如果您想了解更多关于这些参数的信息，您可以参考以下链接：
+- [chatglm常用参数 ：Top-k, Top-p, Temperature - CSDN博客](^1^)
+- [Top-k & Top-p, Temperature - 知乎](^2^)
+- [chatgpt 调用参数解析 - 知乎](^3^)
+
+希望这些信息对您有所帮助。如果您还有其他问题，请随时提出。😊
+
+Source: Conversation with Bing, 7/19/2023
+(1) chatglm常用参数 ：Top-k, Top-p, Temperature - CSDN博客. https://blog.csdn.net/stay_foolish12/article/details/131478570.
+(2) Top-k & Top-p, Temperature - 知乎 - 知乎专栏. https://zhuanlan.zhihu.com/p/613428710.
+(3) chatgpt 调用参数解析 - 知乎 - 知乎专栏. https://zhuanlan.zhihu.com/p/613262543.
+
+
+
+top_k = 1  简单的greedy search
+
+top_p temperature repeat_penalty 都和采样有关
+
+float repeat_penalty = 1.0f; // 重复惩罚系数，1.0代表不惩罚
+float temperature = 1.0; // 温度参数，一般在0.1 ~ 1.0之间，设大这个参数可以带来结果的多样性
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 
 from transformers import AutoTokenizer, AutoModel
 from fastllm_pytools import llm
-tokenizer = AutoTokenizer.from_pretrained("chatglm2_6b", trust_remote_code=True)
-# tokenizer = AutoTokenizer.from_pretrained("/home/faith/chatglm2-6b", trust_remote_code=True)
+# tokenizer = AutoTokenizer.from_pretrained("chatglm2_6b", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("/home/faith/chatglm2-6b", trust_remote_code=True)
 
 # tokenizer = AutoTokenizer.from_pretrained("/home/faith/fastllm/chatglm2-6b-int4", trust_remote_code=True)
 import time
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # model = AutoModel.from_pretrained("/home/faith/chatglm2-6b", trust_remote_code=True)
 # model = llm.from_hf(model, tokenizer, dtype="float16")  # 可以调整为"float16", "int8", "int4"
 
@@ -20,12 +20,12 @@ text = '''已知信息：
     以下内容都是提问的设备3045的相关信息:15.照明装置设备整体结构大概可分为 部分组成整机重量 吨为方便设备运输或移动给料斗可升降尾料输送机可折叠侧面楼梯可折叠本章只记录主要零部件如想查阅更详细的零部件信息请参考KJ-3045B 机架图片的链接是: http://img.com/cf9e206b436a624a0f5f89e6f24eab16.png-3序号 10按键可以控制柴油机的速度4.03 设备形态转换当设备移动到指定地点后便可从运输状态图 4.03-1转换为工作状态图 57 -图片的链接是: http://img.com/273bdbe9b0ad0dd0fe687a4c1d0b3261.png 图 4.03-1 设备运输状态图片的链接是: http://img.com/2bf1e565bf2f16f849f65458600ee3fa.png 图 4.03-2 设备工作状态 58 - 尾料输送机工作状态/运 输状态装换一拆除运输固定装置拆除尾料输送机运输固定螺栓图 -1序号 1图片的链接是: http://img.com/baab6a53793c8430be0b03272ae3816b.png 1.尾料输送机运输固定螺栓图 -1 尾料输送机运输状态二展开尾料输送机1.展开 尾料输送机一段操作遥控器将尾料输送机一段摇杆图 -3缓缓下拨此时尾料输送机一段油7才能使柴油机停止工作手动模式下遥控器无法控制柴油机停止自动模式下只需要按一下遥控器上的柴油机停止按钮-3序号 8柴油机就会减速直至停止如需要更详细的介绍请参照柴油机控制柜使用说明书4.04 设备安装设备安装只能由我公司售后服务工程师或受过专业操作技能培训的人员获得用户授权后方可进行安装作业 
 
     根据上述已知信息，简洁和专业回答用户的问题，如果问题里询问图片，请返回相关的图片具体链接。如果已知信息里有多个图片，请返回最匹配的图片链接，并用[]包含链接内容而且不要有其他文字描述。
-    如果无法从中得到答案，请说 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”，不允许在答案中添加编造成分，答案请使用中文。 问题是：3045的运输状态图片'''
+    如果无法从中得到答案，请说 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”，不允许在答案中添加编造成分，答案请使用中文。 问题是：3045的设备运输状态图片'''
 outs = ""
 history = [['你好', '你好，我是机器人']]
 for i in range(1):
     _start = time.time()
-    out = model.response(text, do_sample=False, temperature=1.0, history=history)
+    out = model.response(text, do_sample=False, temperature=1.0, history=history, top_k=1, top_p=0.5, repeat_penalty=1)
     outs += out
 
     print("throughput: {} {}".format((len(tokenizer.tokenize(out)) / (time.time() - _start)), "tokens/s"))
